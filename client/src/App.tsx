9ffe604ff5d112b8ebc2163d456ac85acf211dd3
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2025-08-13 15:27:52 -0600

    Add basic users table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useUsers } from './hooks/useUsers';
-import { Box, Container, Tabs, Text } from '@radix-ui/themes';
+import { Avatar, Box, Container, Flex, Table, Tabs, Text } from '@radix-ui/themes';
+import { formatDate } from './util/formatDate';
 
 function App() {
   const { data: users, loading, error } = useUsers();
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Container size="4">
+      <Container size="4" py={{ initial: '4', lg: '8' }} px="4">
         <Tabs.Root defaultValue="users">
           <Tabs.List>
             <Tabs.Trigger value="users">Users</Tabs.Trigger>
@@ -18,15 +19,43 @@ function App() {
 
           <Box pt="3">
             <Tabs.Content value="users">
-              <ul>
-                {users.map(user => (
-                  <li key={user.id}>
-                    <Text size="2">
-                      {user.first} {user.last}
-                    </Text>
-                  </li>
-                ))}
-              </ul>
+              <Table.Root variant="surface">
+                <Table.Header>
+                  <Table.Row>
+                    <Table.ColumnHeaderCell>User</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>Role</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>Joined</Table.ColumnHeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  {users.map(user => (
+                    <Table.Row key={user.id}>
+                      <Table.RowHeaderCell>
+                        <Flex align="center" gap="2">
+                          <Avatar
+                            src={user.photo}
+                            fallback={`${user.first[0]}${user.last[0]}`}
+                            radius="full"
+                            size="2"
+                          />
+                          <Text size="2">
+                            {user.first} {user.last}
+                          </Text>
+                        </Flex>
+                      </Table.RowHeaderCell>
+                      <Table.Cell>{user.role?.name || 'Unknown'}</Table.Cell>
+                      <Table.Cell>
+                        {formatDate(user.createdAt, {
+                          year: 'numeric',
+                          month: 'long',
+                          day: '2-digit',
+                        })}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table.Root>
             </Tabs.Content>
 
             <Tabs.Content value="roles">
